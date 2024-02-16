c01ce879659cd6b29c50692c5b28030c4e3dbc3e
claudiuvilau <64689038+claudiuvilau@users.noreply.github.com>
2024-02-16 18:30:28 +0100

    first commit

---
+++ src/main/java/com/openclassrooms/medilabosolution/MedilabosolutionApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.medilabosolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedilabosolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedilabosolutionApplication.class, args);
+	}
+
+}
